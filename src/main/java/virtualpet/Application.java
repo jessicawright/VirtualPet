fdb1918d4733e14d9543fb78973b5d34b88c232c
Chad <ch.collins@outlook.com>
2019-01-24 15:15:36 -0500

    cleaned user menu. Finalized pet attributes.

--- src/main/java/virtualpet/Application.java
+++ src/main/java/virtualpet/Application.java
@@ -7,10 +7,9 @@ public class Application {
 	public static void main(String[] args) {
 
 		Scanner userInput = new Scanner(System.in);
-		
 
 		// test user created pet name
-		System.out.println("Enter pet name");
+		System.out.println("Hello! Please enter a name for your new pet.");
 		String userPetName = userInput.nextLine();
 		System.out.println(userPetName);
 
@@ -19,40 +18,71 @@ public class Application {
 		System.out.println("DOG, CAT, RABBIT");
 		String userPetType = userInput.nextLine().toLowerCase().trim();
 		// trim() method eliminates leading and trailing spaces
-		
+
 		while (!userPetType.equals("dog") && !userPetType.equals("cat") && !userPetType.equals("rabbit")) {
 			System.out.println("Please try again...");
 			userPetType = userInput.nextLine().toLowerCase().trim();
-		} 
+		}
 
-		Pet unnamed = new Pet(userPetName, userPetType, 10, true,100); // 0 not hungry at all
-		System.out.println(unnamed.getPetName() + " " + unnamed.getPetType() + " " + unnamed.getpetFullness());
+		Pet unnamed = new Pet(userPetName, userPetType, 50, true, 50, 50, 50); // 0 not hungry at all
+		System.out.println("Say hello to " + unnamed.getPetName() + ", your new " + unnamed.getPetType() + ".");
+		System.out.println("----------");
 		// end user pet creation
 
 		while (unnamed.petAlive()) {
 
 			unnamed.incrementStatus();
-			
-			System.out.println("what would you like to do?");
-			System.out.println("1. check pet status");
-			System.out.println("2. feed pet");
-			System.out.println("3. play with pet");
-			System.out.println("4. clean pet");
 
-			System.out.println("Press 2 to feed");
-			int userMenuChoice = userInput.nextInt();
+			System.out.println("what would you like to do?");
+			System.out.println();
+			System.out.println("----------");
+			System.out.println("Pet Status:");
+			System.out.println("Fullness = " + unnamed.getPetFullness());
+			System.out.println("Cleanliness = " + unnamed.getPetCleanliness());
+			System.out.println("Energy = " + unnamed.getPetEnergy());
+			System.out.println("Happiness = " + unnamed.getPetHappiness()); // create method petStatus?
+			System.out.println("----------");
+			System.out.println();
+			System.out.println("1. feed pet.");
+			System.out.println("2. play with pet");
+			System.out.println("3. clean pet");
+			System.out.println("4. Accept new pet");
+
+			String userMenuChoice = userInput.nextLine();
 
 			System.out.println(userMenuChoice);
-			if (userMenuChoice == 2) {
-				unnamed.feedPet();
-				System.out.println("new hunger status: " + unnamed.getPetFullness());
-			
-			}
-		}
-		
-		System.out.println("Your pet died");
-		System.out.println("Good job, your pet is clean!");
-	}
-	
+			switch (userMenuChoice) {
 
-}
+			case "1":
+				unnamed.feedPet();
+				break;
+			case "2":
+				unnamed.playPet();
+				break;
+			case "3":
+				unnamed.cleanPet();
+				break;
+			case "4":
+				//System.out.println("Enter pet name");
+				//userPetName = userInput.nextLine();
+				//System.out.println(userPetName);
+
+				// test user chosen pet type from list
+				///System.out.println("Please choose a pet type");
+				//System.out.println("DOG, CAT, RABBIT");
+				//userPetType = userInput.nextLine().toLowerCase().trim();
+				///trim() method eliminates leading and trailing spaces
+
+				//while (!userPetType.equals("dog") && !userPetType.equals("cat") && !userPetType.equals("rabbit")) {
+					//System.out.println("Please try again...");
+					//userPetType = userInput.nextLine().toLowerCase().trim();
+				System.out.println("Not available yet");
+					break;
+
+				}
+			}
+
+			System.out.println("Your pet died.");
+		}
+	}
+
