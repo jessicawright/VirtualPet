174fa4ebf20a1a5854814168fbca64838f4fc338
jessicawright <wright.983@gmail.com>
2019-01-28 11:59:41 -0500

    changed pet methods to shelter methods and rearranged

--- src/main/java/virtualpet/PetShelter.java
+++ src/main/java/virtualpet/PetShelter.java
@@ -5,19 +5,59 @@ import java.util.HashMap;
 public class PetShelter {
 
 	private HashMap<String, Pet> pets = new HashMap<String, Pet>();
+	
+	
+	
 
 	public void addPet(Pet virtualPet) {
 		pets.put(virtualPet.getPetName(), virtualPet);
 
 	}
+	
+	public Pet getPet(String petToGet) {
+		return pets.get(petToGet);
+	}
 
 	public int getPetsLength() {
-
 		return pets.size();
 	}
 
 	public void removePet(Pet underTestPet) {
 		pets.remove(underTestPet.getPetName(), underTestPet);
+
+	}
+	
+	public void feedAllPets() {
+		for (Pet pet : pets.values()) {
+			pet.feedPet();
+		}
+	}
+	
+	public void cleanAllPets() {
+		for (Pet pet : pets.values()) {
+			pet.cleanPet();
+		}
+	}
+	
+	public void playAllPets() {
+		for (Pet pet : pets.values()) {
+			pet.playPet();
+		}
+	}
+	
+	public void sleepAllPets() {
+		for (Pet pet : pets.values()) {
+			pet.sleepPet();
+		}
+	}
+	
+	public void incrementAllPets() {
+		for (Pet pet : pets.values()) {
+			pet.incrementStatus();
+		}
+	}
+		
+	
 
 	}
 
